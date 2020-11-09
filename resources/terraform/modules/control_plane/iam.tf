307bdc3bc7683b013c10922403ed29a4bbb5a68d
atsikham <atsikham@users.noreply.github.com>
2020-11-09 23:54:16 +0100

    Added control_plane submodule

---
+++ resources/terraform/modules/control_plane/iam.tf
@@ -0,0 +1,30 @@
+# https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html
+
+resource "aws_iam_role" "eks_cluster_iam_role" {
+  name = "${var.name}-eks-cluster-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "cluster_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = ["eks.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_iam_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_iam_role.name
+}
+
+# Enable security groups for pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "eks_iam_vpc_resource_controller_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_iam_role.name
+}
