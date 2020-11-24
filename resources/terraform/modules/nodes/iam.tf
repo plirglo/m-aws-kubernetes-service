00d02d97bf5e667a1a5e2d9b8c85748f15acf873
Anatoli Tsikhamirau <anatoliytihomirov@yahoo.com>
2020-11-24 14:09:29 +0100

    Changed nodes iam role name
    Co-authored-by: to-bar <46519524+to-bar@users.noreply.github.com>

--- resources/terraform/modules/nodes/iam.tf
+++ resources/terraform/modules/nodes/iam.tf
@@ -1,7 +1,7 @@
 # https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html
 
 resource "aws_iam_role" "eks_nodes_iam_role" {
-  name = "${var.name}-eks-node-group"
+  name = "${var.name}-eks-nodes-iam-role"
   assume_role_policy = data.aws_iam_policy_document.nodes_assume_role_policy.json
 }
 
