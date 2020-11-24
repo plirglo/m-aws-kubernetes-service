a62447eade50cc0d2a43347364bb3ad407fd2cb1
Anatoli Tsikhamirau <anatoliytihomirov@yahoo.com>
2020-11-24 13:58:50 +0100

    Changed CA data variable name
    Co-authored-by: to-bar <46519524+to-bar@users.noreply.github.com>

--- resources/terraform/modules/control_plane/kubeconfig.tf
+++ resources/terraform/modules/control_plane/kubeconfig.tf
@@ -2,7 +2,7 @@ data "template_file" "kubeconfig" {
   template = file("${path.module}/templates/kubeconfig.tpl")
   vars     = {
     endpoint     = aws_eks_cluster.eks_cluster.endpoint
-    certificate  = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+    certificate_data  = aws_eks_cluster.eks_cluster.certificate_authority[0].data
     cluster_name = aws_eks_cluster.eks_cluster.name
   }
 }
